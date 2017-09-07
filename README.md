74efad9e229e76d00562ecaea661598402d2570f
Aaron Gutierrez <aarongutierrez@asana.com>
2017-09-07 14:52:05 -0700

    Formatting mostly works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # asana-markdown
 ## chrome extension to add markdown formatting in asana
+
+Known issues:
+ * `__*bold italic text*__` does not work properly
---
+++ asana-markdown.js
@@ -0,0 +1,29 @@
+function formatDocument() {
+  const description = document.getElementsByClassName("TaskDescription")[0];
+  const stories = document.getElementsByClassName("RichText");
+
+  formatText(description);
+
+  for (let i=0; i<stories.length; i++) {
+    formatText(stories[i]);
+  }
+}
+
+function formatText(elem) {
+  const title_regex = /#+ ([^<]+)(?=<\/)/g;
+  const italic_regex = /_([^_<"]+)_/g;
+  const italic_star_regex = /\*([^\*<"]+)\*/g;
+  const bold_regex = /__([^_])__/g;
+  const bold_star_regex = /\*\*([^\*]+)\*\*/g;
+  const code_regex = /`([^`]+)`/g;
+  const code_block_regex = /```([^`]+)```/g;
+
+  elem.innerHTML = elem.innerHTML
+    .replace(code_block_regex, "<code>$1</code>")
+    .replace(code_regex, "<code>$1</code>")
+    .replace(title_regex, "<b>$1</b>")
+    .replace(bold_regex, "<b>$1</b>")
+    .replace(italic_regex, "<i>$1</i>")
+    .replace(bold_star_regex, "<b>$1</b>")
+    .replace(italic_star_regex, "<i>$1</i>");
+}
