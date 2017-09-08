d1b3679a99dcf6a03bc44eb0275b829b891740d0
Aaron Gutierrez <aarongutierrez@asana.com>
2017-09-07 17:25:07 -0700

    DOM diffing to determine when to updates
    Also makes the regex more strict to avoid matching underscore_case variables

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # asana-markdown
 ## chrome extension to add markdown formatting in asana
 
+Performs rich text formatting after rendering. Is a rough approximation of
+reactive, and currently supports **bold**, _italic_, and `code` formatting.
+
 Known issues:
- * `__*bold italic text*__` does not work properly
- * doesn't reformat after expanding the story feed
+ * `_"italic quotes"_` are not supported to avoid a problem with underscores being somewhat common
+ * occasional race conditions with react
--- asana-markdown.js
+++ asana-markdown.js
@@ -1,22 +1,25 @@
-function formatDocument() {
-  const description = document.getElementsByClassName("TaskDescription")[0];
+function elementsToFormat() {
+  const description = document
+    .getElementsByClassName("TaskDescription-textEditor")[0];
   const stories = document.getElementsByClassName("RichText");
 
-  if (description) {
-    formatText(description);
-  }
+  const elements = description && !description.classList.contains("is-focused")
+    ? [description]
+    : [];
 
   for (let i=0; i<stories.length; i++) {
-    formatText(stories[i]);
+    elements.push(stories[i]);
   }
+
+  return elements;
 }
 
-function formatText(elem) {
-  const title_regex = /#+ ([^<]+)(?=<\/)/g;
-  const italic_regex = /_([^_<"]+)_/g;
-  const italic_star_regex = /\*([^\*<"]+)\*/g;
-  const bold_regex = /__([^_])__/g;
-  const bold_star_regex = /\*\*([^\*]+)\*\*/g;
+function formatElem(elem) {
+  const title_regex = /#+ ([^<]+)(?=<\/|<br>)/g;
+  const italic_regex = /(\s|>)_([^_<"]+)_/g;
+  const italic_star_regex = /(\s|>)\*([^\*<"]+)\*/g;
+  const bold_regex = /(\s|>)__([^_])__/g;
+  const bold_star_regex = /(\s|>)\*\*([^\*]+)\*\*/g;
   const code_regex = /`([^`]+)`/g;
   const code_block_regex = /```([^`]+)```/g;
 
@@ -24,21 +27,41 @@ function formatText(elem) {
     .replace(code_block_regex, "<code>$1</code>")
     .replace(code_regex, "<code>$1</code>")
     .replace(title_regex, "<b>$1</b>")
-    .replace(bold_regex, "<b>$1</b>")
-    .replace(italic_regex, "<i>$1</i>")
-    .replace(bold_star_regex, "<b>$1</b>")
-    .replace(italic_star_regex, "<i>$1</i>");
+    .replace(bold_regex, "$1<b>$2</b>")
+    .replace(italic_regex, "$1<i>$2</i>")
+    .replace(bold_star_regex, "$1<b>$2</b>")
+    .replace(italic_star_regex, "$1<i>$2</i>");
 }
 
-var oldLocation;
+var oldDOM = 0;
 
-function locationListener() {
-  if (oldLocation !== window.location.href) {
-    oldLocation = window.location.href;
-    window.setTimeout(formatDocument, 200);
+/* hash function used to do quick-enough dom diffing
+ * from http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method
+ */
+String.prototype.hashCode = function(){
+  var hash = 0;
+  if (this.length == 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    char = this.charCodeAt(i);
+    hash = ((hash<<5)-hash)+char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+function maybeFormatElements() {
+  const elems = elementsToFormat();
+
+  const newDOM = elems.reduce(
+      (accum, elem) => (accum << 5 - accum) + elem.innerHTML.hashCode(), 0);
+
+  if (newDOM !== oldDOM) {
+    elems.map(formatElem);
+    oldDOM = elems.reduce(
+      (accum, elem) => (accum << 5 - accum) + elem.innerHTML.hashCode(), 0);
   }
 
-  window.setTimeout(locationListener, 100);
+  window.setTimeout(maybeFormatElements, 100);
 }
 
-window.addEventListener("load", locationListener);
+window.addEventListener("load", maybeFormatElements);
